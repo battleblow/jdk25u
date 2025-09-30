68ce9ffe1ce122c9a8791cfb4a036091714f7c91
Greg Lewis <glewis@eyesbeyond.com>
2025-09-30 13:04:21 -0700

    Fix crashes on Apple Silicon (#5)
    See explanation in https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=265284#c10
    
    From: NetBSD port (but with additional portability tweaks)
    Authored by: pho@netbsd.org

--- src/hotspot/share/c1/c1_Runtime1.cpp
+++ src/hotspot/share/c1/c1_Runtime1.cpp
@@ -1339,7 +1339,7 @@ void Runtime1::patch_code(JavaThread* current, C1StubId stub_id) {
 
   // Enable WXWrite: the function is called by c1 stub as a runtime function
   // (see another implementation above).
-  MACOS_AARCH64_ONLY(ThreadWXEnable wx(WXWrite, current));
+  BSD_AARCH64_ONLY(ThreadWXEnable wx(WXWrite, current));
 
   if (TracePatching) {
     tty->print_cr("Deoptimizing because patch is needed");
